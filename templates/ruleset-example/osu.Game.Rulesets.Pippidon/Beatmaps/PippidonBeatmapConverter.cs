ce307eaf1002cfe12a505b692dadd3089dd1257c
Dean Herbert <pe@ppy.sh>
2019-09-19 11:27:55 +0900

    Fix incorrect header

--- templates/ruleset-example/osu.Game.Rulesets.Pippidon/Beatmaps/PippidonBeatmapConverter.cs
+++ templates/ruleset-example/osu.Game.Rulesets.Pippidon/Beatmaps/PippidonBeatmapConverter.cs
@@ -5,8 +5,8 @@ using System;
 using System.Collections.Generic;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Objects;
-using osu.Game.Rulesets.Pippidon.Objects;
 using osu.Game.Rulesets.Objects.Types;
+using osu.Game.Rulesets.Pippidon.Objects;
 using osuTK;
 
 namespace osu.Game.Rulesets.Pippidon.Beatmaps
