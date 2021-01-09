34a3643508f669cedb7708d81cfd09a6f1d93c1e
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2021-01-09 15:30:20 +0100

    Fix compilation failures in Pippidon

--- templates/ruleset-example/osu.Game.Rulesets.Pippidon/Objects/Drawables/DrawablePippidonHitObject.cs
+++ templates/ruleset-example/osu.Game.Rulesets.Pippidon/Objects/Drawables/DrawablePippidonHitObject.cs
@@ -42,13 +42,9 @@ namespace osu.Game.Rulesets.Pippidon.Objects.Drawables
             });
         }
 
-        protected override IEnumerable<HitSampleInfo> GetSamples() => new[]
+        public override IEnumerable<HitSampleInfo> GetSamples() => new[]
         {
-            new HitSampleInfo
-            {
-                Bank = SampleControlPoint.DEFAULT_BANK,
-                Name = HitSampleInfo.HIT_NORMAL,
-            }
+            new HitSampleInfo(HitSampleInfo.HIT_NORMAL, SampleControlPoint.DEFAULT_BANK)
         };
 
         protected override void CheckForResult(bool userTriggered, double timeOffset)
